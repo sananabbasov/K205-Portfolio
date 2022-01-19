13e04f2afec743ea9996d36d972af169d3d44e8e
sananabbasov <sanan_abbasov@list.ru>
2022-01-19 16:45:55 +0400

    19.01.2022 17:00

--- K205-Portfolio/Controllers/HomeController.cs
+++ K205-Portfolio/Controllers/HomeController.cs
@@ -1,27 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using K205_Portfolio.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace K205_Portfolio.Controllers
 {
     public class HomeController : Controller
     {
+       
+        List<Product> products= new() {
+            new Product { 
+                Id = 1, 
+                Name = "IPhone 11", 
+                Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." ,
+                PhotoURL = "https://cdn.pixabay.com/photo/2017/03/16/05/23/singapore-2148190_960_720.jpg"
+            },
+            new Product { 
+                Id=2,
+                Name= "Xiomi",
+                Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                PhotoURL = "https://cdn.pixabay.com/photo/2021/12/02/02/59/mountains-6839521_960_720.jpg"
+            }
+        };
+
         public IActionResult Index()
         {
 
             // ViewData, ViewBag ve ViewTemp
 
 
-            var gun = "axsam";
+            //var gun = "axsam";
 
-            var salamlama = "Sabahiniz xeyir";
+            //var salamlama = "Sabahiniz xeyir";
 
-            if (gun != "seher")
-                salamlama = "Axsaminiz xeyir";
+            //if (gun != "seher")
+            //    salamlama = "Axsaminiz xeyir";
 
 
 
-            ViewBag.Salamlama = salamlama;
+            //ViewBag.Salamlama = salamlama;
 
-            
+            ViewData["products"] = products;
+
+
+
             return View();
         }
         public IActionResult About()
---
+++ K205-Portfolio/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace K205_Portfolio.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PhotoURL { get; set; }
+    }
+}
